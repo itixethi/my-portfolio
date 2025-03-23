ea0bf31631287fa9b764b1c5be8fdd9445d0153d
Osabohien Igiehon <Osabohien.igiehon@student.griffith.ie>
2025-03-23 05:46:13 +0000

    route/login js details and some html
    Signed-off-by: Osabohien Igiehon <Osabohien.igiehon@student.griffith.ie>

---
+++ firebase/helpers.py
@@ -0,0 +1,21 @@
+from fastapi.responses import HTMLResponse, JSONResponse
+import google.oauth2.id_token
+from google.auth.transport import requests
+from google.cloud import firestore
+import starlette.status as status
+
+
+firestore_db = firestore.Client()
+
+def validateFirebaseToken(id_token, firebase_request_adapter):
+    if not id_token:
+        return None
+    
+    user_token = None
+
+    try:
+        user_token = google.oauth2.id_token.verify_firebase_token(id_token, firebase_request_adapter)
+    except Exception as e:
+        user_token = None
+    
+    return user_token
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.responses import HTMLResponse, RedirectResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import google.oauth2.id_token
@@ -7,13 +7,14 @@ from google.auth.transport import requests
 from google.cloud import firestore
 import starlette.status as status
 
+from firebase.helpers import validateFirebaseToken
+
 # i call the app i shall use here 
 app = FastAPI()
 firestore_db = firestore.Client()
 firebase_request_adapter = requests.Request()
 
 # i set the static and templates folders here 
-app.get("/")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
@@ -24,3 +25,25 @@ def getUser(user_token):
 
         user.set(user_data)
         return user
+    
+def checkAndReturnUser(id_token):
+    user_token = validateFirebaseToken(id_token=id_token, firebase_request_adapter=firebase_request_adapter)
+    if not user_token:
+        return None
+    
+    return getUser(user_token)
+
+app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    try:
+        id_token = request.cookies.get("token")
+        user_token = validateFirebaseToken(id_token=id_token, firebase_request_adapter=firebase_request_adapter)
+        isAuthorized = user_token is not None
+
+        return templates.TemplateResponse(request=request, name="index.html", context={"isAuthorized": isAuthorized, "request": request, })
+    except Exception as e:
+        return HTMLResponse(str(e), status_code=500)
+    
+@app.get("/login", response_class=HTMLResponse)
+async def handleLoginRoute(request: Request):
+    return await login(request=request, templates=templates)
